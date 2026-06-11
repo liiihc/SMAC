6fe37ca10c79cff6bb44c49ae375f77226c5b828
liiihc <158269516+liiihc@users.noreply.github.com>
2026-06-11 16:45:30 +0800

    09B-6mApT_features.pl

--- SMAC-v1.0/09B-6mApT_features.pl
+++ SMAC-v1.0/09B-6mApT_features.pl
@@ -3,25 +3,30 @@ open (IN2,"<","$ARGV[0]");
 open (IN3,"<","$ARGV[0]");
 open (IN4,"<","$ARGV[0]");
 system "mkdir $ARGV[1]";
-$CUTOFFA=2**$ARGV[2];
-$CUTOFFW=2**$ARGV[3];
-$CUTOFFC=2**$ARGV[4];
-$CPU=$ARGV[5];
+open (IN5,"<","$ARGV[2]");
+$CUTOFFA=2**$ARGV[3];
+$CUTOFFW=2**$ARGV[4];
+$CUTOFFC=2**$ARGV[5];
+$CPU=$ARGV[6];
 open (OUT1,">","$ARGV[1]/${CPU}_6mAratio");
 open (OUT2,">","$ARGV[1]/${CPU}_6mAorA_inApT_doubleCutoff");
 open (OUT3,">","$ARGV[1]/${CPU}_6mAorA_inApAGC_singleCutoff");
 open (OUT4,">","$ARGV[1]/${CPU}_6mAorA_inApO_singleCutoff");
 open (OUT5,">","$ARGV[1]/${CPU}_6mAorA_inApT_singleStrand_singleCutoff");
 $num_AT=0;
+while (defined ($SD=<IN5>)){
+	if ($SD=~/(.+)/){
+		$SD{$1}=1}}
 while (defined ($A=<IN1>)){
-	$num_A++;}
+	if ($A=~/^(\d+)\s+/ && !exists $SD{$1}){
+	$num_A++;}}
 while (defined ($A=<IN2>)){
-	if ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+(.)\s+(.+)\s+AT\s+/){
+	if ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+(.)\s+(.+)\s+AT\s+/ && !exists $SD{$1}){
 		$pos=join "~","$1","$2","$3","$4";
 		$hash{$pos}=$5;
 		}}
 while (defined ($A=<IN3>)){
-	if ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+(.)\s+(.+)\s+AT\s+(.+)/){
+	if ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+(.)\s+(.+)\s+AT\s+(.+)/ && !exists $SD{$1}){
 		if ($4 eq "W"){$pos=$3+1;
                 	$pos1=join "~","$1","$2","$pos","C";
                 	if (! exists $hash{$pos1} && $5>=$CUTOFFA){print OUT5 "$1\t$2\t$3\t$4\t6mA\tAT\t$6\n";$num_6mApN++}
@@ -30,14 +35,14 @@ while (defined ($A=<IN3>)){
 			$pos1=join "~","$1","$2","$pos","W";
                 	if (! exists $hash{$pos1} && $5>=$CUTOFFA){print OUT5 "$1\t$2\t$3\t$4\t6mA\tAT\t$6\n";$num_6mApN++}
                 	elsif (! exists $hash{$pos1} && $5<$CUTOFFA){print OUT5 "$1\t$2\t$3\t$4\tno6mA\tAT\t$6\n"}}}
-	elsif ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+(.)\s+(.+)\s+AN\s+(.+)/){
+	elsif ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+(.)\s+(.+)\s+AN\s+(.+)/ && !exists $SD{$1}){
 		if ($5>=$CUTOFFA){print OUT4 "$1\t$2\t$3\t$4\t6mA\tAN\t$6\n";$num_6mApN++}
 		else {print OUT4 "$1\t$2\t$3\t$4\tno6mA\tAN\t$6\n"}}
-	elsif ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+(.)\s+(.+)\s+(A[G|C|A])\s+(.+)/){
+	elsif ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+(.)\s+(.+)\s+(A[G|C|A])\s+(.+)/ && !exists $SD{$1}){
 		if ($5>=$CUTOFFA){print OUT3 "$1\t$2\t$3\t$4\t6mA\t$6\t$7\n";$num_6mApN++}
                 else {print OUT3 "$1\t$2\t$3\t$4\tno6mA\t$6\t$7\n"}}}
 while (defined ($A=<IN4>)){
-	if ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+W\s+(.+)\s+AT\s+(.+)/){
+	if ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+W\s+(.+)\s+AT\s+(.+)/ && !exists $SD{$1}){
 		$pos=$3+1;
 		$pos1=join "~","$1","$2","$pos","C";
 		if (exists $hash{$pos1}){$num_AT+=2;
@@ -45,7 +50,7 @@ while (defined ($A=<IN4>)){
 		elsif($4>=$CUTOFFA && $hash{$pos1}<$CUTOFFC){$hemi_W++;$non+=1;print OUT2 "$1\t$2\t$3\tW\t6mA\tAT\t$5\n"}
 		elsif($4<$CUTOFFW && $hash{$pos1}>=$CUTOFFA){$hemi_C++;$non+=1;print OUT2 "$1\t$2\t$3\tW\tno6mA\tAT\t$5\n"}
 		else {print OUT2 "$1\t$2\t$3\tW\t6mA\tAT\t$5\n"}}}
-	elsif ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+C\s+(.+)\s+AT\s+(.+)/){
+	elsif ($A=~/(\d+)\s+(.+?)\s+(\d+)\s+C\s+(.+)\s+AT\s+(.+)/ && !exists $SD{$1}){
                 $pos=$3-1;
                 $pos1=join "~","$1","$2","$pos","W";
                 if (exists $hash{$pos1}){
